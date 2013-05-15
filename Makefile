52ae4541cf11ad0cde55d045973555a82943e5da
bkgoodman@gmail.com <bkgoodman@gmail.com@50288caa-fb7e-d49a-bd86-818ccbed9132>
2013-05-15 22:56:20 +0000

    Added true "two factor" authenticaton - allowing a static password to be specified in the user's key file
    Also added runtime definable log level.
    
    
    
    git-svn-id: http://google-authenticator-apache-module.googlecode.com/svn/trunk@21 50288caa-fb7e-d49a-bd86-818ccbed9132

--- Makefile
+++ Makefile
@@ -1,17 +1,35 @@
+APACHE_CONFIG=/etc/httpd
+APACHE_BIN=/usr/sbin/httpd
 APXS=apxs
+PWD=$(shell pwd)
+AXPS_FLAGS=-Wc,-fdump-rtl-expand
 SOURCE=mod_authn_google.c base32.c hmac.c sha1.c
 .FORCE: all
 all: $(SOURCE)
-	$(APXS) -c $^
+	$(APXS) $(AXPS_FLAGS) -c $^
+
+make32: $(SOURCE)
+	$(APXS) -Wc,-m32 -c $^
 
 install: all
-	 sudo cp .libs/mod_authn_google.so /usr/local/apache2/modules/
+	 sudo cp .libs/mod_authn_google.so /etc/httpd/modules/
 
 clean:
-	rm -rf .libs/ *.o *.so *.la *.slo *.lo
+	rm -rf .libs/ *.o *.so *.la *.slo *.lo *.expand
+
+debug: $(SOURCE)
+	$(APXS) -D DEBUG=1 -c $^
 
 test:
 	make install
 	sleep 1
-	sudo service httpd restart
+	if [ ! -f ${APACHE_CONFIG}/ga_auth/test ]  ; then  \
+		sudo mkdir -p ${APACHE_CONFIG}/ga_auth/ ; \
+		sudo cp test.gaconf ${APACHE_CONFIG}/ga_auth/test ; \
+	fi
 	
+	sudo ${APACHE_BIN} -e debug -X -f $(PWD)/httpd.conf &
+	sudo tail -f /var/log/httpd/error_log
+	
+
+
--- mod_authn_google.c
+++ mod_authn_google.c
@@ -48,10 +48,12 @@
 #define DEBUG
 
 ap_regex_t *cookie_regexp;
+ap_regex_t *passwd_regexp;
 typedef struct {
     char *pwfile;
 		int cookieLife;
 		int entryWindow;
+		int debugLevel;
 } authn_google_config_rec;
 
 
@@ -90,6 +92,7 @@ static void *create_authn_google_dir_config(apr_pool_t *p, char *d)
     conf->pwfile = NULL;     /* just to illustrate the default really */
 		conf->cookieLife=0;
 		conf->entryWindow=0;
+		conf->debugLevel=0;
     return conf;
 }
 
@@ -115,7 +118,10 @@ static const command_rec authn_google_cmds[] =
                    OR_AUTHCFG, "Directory containing Google Authenticator credential files"),
     AP_INIT_TAKE1("GoogleAuthCookieLife", set_authn_set_int,
                    (void *)APR_OFFSETOF(authn_google_config_rec, cookieLife),
-                   OR_AUTHCFG, "Enable authentication cookies with lifespan given in seconds"),
+                   OR_AUTHCFG, "Life (in seconds) authentication cookie before revalidation required"),
+    AP_INIT_TAKE1("GoogleAuthLogLevel", set_authn_set_int,
+                   (void *)APR_OFFSETOF(authn_google_config_rec, debugLevel),
+                   OR_AUTHCFG, "Verbosity level of debug output (zero=off)"),
     AP_INIT_TAKE1("GoogleAuthEntryWindow", set_authn_set_int,
                    (void *)APR_OFFSETOF(authn_google_config_rec, entryWindow),
                    OR_AUTHCFG, "Enable authentication cookies with lifespan given in seconds"),
@@ -134,21 +140,36 @@ static char * hash_cookie(apr_pool_t *p, uint8_t *secret,int secretLen,unsigned 
 		return encoded;
 }
 
-static char *getSharedKey(request_rec *r,char *filename) {
+static char *getSharedKey(request_rec *r,char *filename, char **static_pw) {
+		ap_regmatch_t regmatch[AP_MAX_REG_MATCH];
     char l[MAX_STRING_LEN];
 		char *sharedKey = 0L;
 		apr_status_t status;
     ap_configfile_t *f;
 
+		authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config, &authn_google_module);
     status = ap_pcfg_openfile(&f, r->pool, filename);
+ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
+"OPENING FILENAME %s",filename);
 
     if (status != APR_SUCCESS) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
-                      "check_password: Could not open password file: %s", filename);
+                      "check_password: Couldn't open password file: %s", filename);
         return 0L;
     }
 
     while (!(ap_cfg_getline(l, MAX_STRING_LEN, f))) {
+
+				/* If we have a password entry - return it, if the caller has asked for it */
+
+				if ((static_pw) && (!ap_regexec(passwd_regexp, l, AP_MAX_REG_MATCH, regmatch, 0))) {
+					*static_pw  = ap_pregsub(r->pool, "$1", l,AP_MAX_REG_MATCH,regmatch);
+
+if (conf->debugLevel) 
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+           "Using static password \"%s\"",*static_pw);
+				}
+
         /* Skip comment or blank lines. */
         if ((l[0] == '"') || (!l[0])) {
             continue;
@@ -170,13 +191,15 @@ static char *getSharedKey(request_rec *r,char *filename) {
   * \param secret Secret key returned here. Must be allocated by caller
   * \return Pointer to secret key data on success, NULL on error
  **/
-static uint8_t *getUserSecret(request_rec *r, const char *username, int *secretLen) {
+static uint8_t *getUserSecret(request_rec *r, const char *username, int *secretLen, char **static_pw) {
     authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config,
                                                        &authn_google_module);
 		char *ga_filename = apr_psprintf(r->pool,"%s/%s",conf->pwfile,username);
 		char *sharedKey;
 
-		sharedKey = getSharedKey(r,ga_filename);
+ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+"getUserSecret with username \"%s\"\n",username);
+		sharedKey = getSharedKey(r,ga_filename,static_pw);
 		if (!sharedKey)
         return 0L;
 
@@ -187,6 +210,7 @@ static uint8_t *getUserSecret(request_rec *r, const char *username, int *secretL
 		uint8_t *secret = get_shared_secret(r,sharedKey,secretLen);
 		return secret;
 }
+
 
 
 
@@ -202,53 +226,84 @@ static uint8_t *getUserSecret(request_rec *r, const char *username, int *secretL
  **/
 
 static int find_cookie(request_rec *r,char **user,uint8_t *secret,int secretLen) {
-	char *cookie=0L;
-	char *cookie_expire=0L;
-	char *cookie_valid=0L;
+	char *cookie=0l;
+	char *cookie_expire=0l;
+	char *cookie_valid=0l;
 	ap_regmatch_t regmatch[AP_MAX_REG_MATCH];
-	//authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config, &authn_google_module);
+	authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config, &authn_google_module);
 	cookie = (char *) apr_table_get(r->headers_in, "Cookie");
 	if (cookie) {
+	if (conf->debugLevel) 
+					ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+												"Found cookie \"%s\"",cookie);
 		if (!ap_regexec(cookie_regexp, cookie, AP_MAX_REG_MATCH, regmatch, 0)) {
 			if (user) *user  = ap_pregsub(r->pool, "$2", cookie,AP_MAX_REG_MATCH,regmatch);
 			cookie_expire = ap_pregsub(r->pool, "$3", cookie,AP_MAX_REG_MATCH,regmatch);
 			cookie_valid = ap_pregsub(r->pool, "$4", cookie,AP_MAX_REG_MATCH,regmatch);
 				
-#ifdef DEBUG
+if (conf->debugLevel) 
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "Found cookie Expires \"%s\" Valid \"%s\"",cookie_expire,cookie_valid);
-#endif
+
 				if (cookie_expire && cookie_valid && *user) {
 					long unsigned int exp = apr_atoi64(cookie_expire);
 					long unsigned int now = apr_time_now()/1000000;
 					if (exp < now) {
-#ifdef DEBUG
+
+if (conf->debugLevel) 
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
            "Expired. Now=%lu Expire=%lu\n",now,exp);
-#endif
+
 						return 0;	/* Expired */
 					}
 					if (!secret) {
-						secret = getUserSecret(r,*user,&secretLen);
+						secret = getUserSecret(r,*user,&secretLen,0L);
 					}
 					char *h = hash_cookie(r->pool,secret,secretLen,exp);
-#ifdef DEBUG
+
+if (conf->debugLevel) 
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "Match cookie \"%s\" vs  \"%s\"",h,cookie_valid);
-#endif
+
 					if (apr_strnatcmp(h,cookie_valid)==0)
 						return 1; /* Valid Cookie */
 					else {
-#ifdef DEBUG
+
+if (conf->debugLevel) 
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "MISMATCHED  cookie \"%s\" vs  \"%s\"",h,cookie_valid);
-#endif
+
 						return 0; /* Mismatched */
 					}
 				}
 		}
 	}
 	return 0;	/* Not found */
+}
+void cookie_test(apr_pool_t *pool,char *cookie) {
+	char *user=0l;
+	char *cookie_expire=0l;
+	char *cookie_valid=0l;
+	ap_regmatch_t regmatch[AP_MAX_REG_MATCH];
+	//authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config, &authn_google_module);
+	if (!ap_regexec(cookie_regexp, cookie, AP_MAX_REG_MATCH, regmatch, 0)) {
+		user  = ap_pregsub(pool, "$2", cookie,AP_MAX_REG_MATCH,regmatch);
+		cookie_expire = ap_pregsub(pool, "$3", cookie,AP_MAX_REG_MATCH,regmatch);
+		cookie_valid = ap_pregsub(pool, "$4", cookie,AP_MAX_REG_MATCH,regmatch);
+		printf("COOKIE GOOD: \"%s\" \"%s\" \"%s\"\n",user,cookie_expire,cookie_valid);
+	} else
+		printf ("INVALID COOKIE: \"%s\"\n",cookie);
+}
+void password_test(apr_pool_t *pool,char *cookie) {
+	char *user=0l;
+	ap_regmatch_t regmatch[AP_MAX_REG_MATCH];
+	printf("will regexec \n");
+	if (!ap_regexec(passwd_regexp, cookie, AP_MAX_REG_MATCH, regmatch, 0)) {
+		printf("regexec done\n");
+		user  = ap_pregsub(pool, "$1", cookie,AP_MAX_REG_MATCH,regmatch);
+		printf("PASSWORD GOOD: \"%s\"\n",user);
+	} else
+		printf ("INVALID PASSWORD: \"%s\"\n",cookie);
 }
 
 static unsigned int computeTimeCode(unsigned int tm,unsigned char *secret, int secretLen) {
@@ -285,10 +340,11 @@ static void addCookie(request_rec *r, uint8_t *secret, int secretLen) {
 		unsigned long exp = (apr_time_now() / (1000000) ) + conf->cookieLife;
 		char *h = hash_cookie(r->pool,secret,secretLen,exp);
 		char * cookie = apr_psprintf(r->pool,"google_authn=%s:%lu:%s",r->user,exp,h);
-#ifdef DEBUG
+
+if (conf->debugLevel)
 		ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
 			"Created cookie expires %lu hash is %s Cookie: %s",exp,h,cookie);
-#endif
+
 		apr_table_addn(r->headers_out,"Set-Cookie",cookie);
 	}
 }
@@ -297,22 +353,26 @@ static void addCookie(request_rec *r, uint8_t *secret, int secretLen) {
 static authn_status ga_check_password(request_rec *r, const char *user,
                                    const char *password)
 {
+	/* Per Apache mod_auth.h: 
+	 * Given a username and password, expected to return AUTH_GRANTED
+	 * if we can validate this user/password combination.
+	 */
     authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config,
                                                        &authn_google_module);
     apr_status_t status=0;
-		char *sharedKey=0L;
 		int tm;
 		int i;
 		unsigned int truncatedHash = 0;
+		char *static_pw=0L;
+		char *correctpw=0L;
 
-#ifdef DEBUG
+if (conf->debugLevel) 
     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
       "**** PW AUTH at  T=%lu  user  \"%s\"",apr_time_now()/1000000,user);
-#endif
 
 
 		int secretLen;
-		uint8_t *secret = getUserSecret(r,user,&secretLen);
+		uint8_t *secret = getUserSecret(r,user,&secretLen,&static_pw);
 		if (!secret) 
 			return AUTH_DENIED;
 		
@@ -323,30 +383,31 @@ static authn_status ga_check_password(request_rec *r, const char *user,
 		 ***/
 		tm  = get_timestamp();
 
-#ifdef DEBUG
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
-      "Secret Key is \"%s\" @ T=%d",sharedKey,tm);
-#endif
-
-		int code = (int) apr_atoi64(password);
+		//int code = (int) apr_atoi64(password);
 		for (i = -(conf->entryWindow); i <= (conf->entryWindow); ++i) {
 		truncatedHash = computeTimeCode(tm+i,secret,secretLen);
-#ifdef DEBUG
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
-      "Checking codes  @ T=%d \"%d\" vs.  \"%d\"",tm,truncatedHash,code);
-#endif
 
-			if (truncatedHash == (unsigned int)code) {
+		if (static_pw) {
+			correctpw = apr_pstrcat(r->pool,static_pw,
+				apr_psprintf(r->pool,"%6.6d",truncatedHash),0L);
+		} else {
+			correctpw =  apr_psprintf(r->pool,"%6.6d",truncatedHash);
+		}
+
+if (conf->debugLevel) 
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
+      "Comparing Authentication   @ T=%d Code=%d \"%s\" vs.  \"%s\"",tm,truncatedHash,password,correctpw);
+
+			if (!apr_strnatcmp(correctpw,password)) {
 				/**\todo  - check to see if time-based code has been invalidated */
 				addCookie(r,secret,secretLen);
 				return AUTH_GRANTED;
 			}
 		}
 
-#ifdef DEBUG
+if (conf->debugLevel) 
     ap_log_rerror(APLOG_MARK, APLOG_ERR, status, r,
-      "Validating for  \"%s\" Shared Key  \"%s\"",password,sharedKey);
-#endif
+      "DENYING for  \"%s\"",password);
 
 
     return AUTH_DENIED;
@@ -366,29 +427,36 @@ static char *hex_encode(apr_pool_t *p, uint8_t *data,int len) {
 }
 
 
-/* This handles Digest Authentication. Returns a has of the 
+/* This handles Digest Authentication. Returns a hash of the 
    User, Realm and (Required) Password. Caller (Digest module)
 	 determines if the entered password was actually valid
 */
 static authn_status ga_get_realm_hash(request_rec *r, const char *user,
                                    const char *realm, char **rethash)
 {
+	/* Per Apache mod_auth.h:
+	*  Given a user and realm, expected to return AUTH_USER_FOUND if we
+	*  can find a md5 hash of 'user:realm:password'
+	*/
     authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config,
                                                        &authn_google_module);
 		char *sharedKey;
 		char *ga_filename;
+		char *static_pw;
 
-#ifdef DEBUG
+if (conf->debugLevel)
     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
       "**** DIGEST AUTH at  T=%lu  user  \"%s\"",apr_time_now()/1000000,user);
-#endif
+
 
 		unsigned char *hash = apr_palloc(r->pool,APR_MD5_DIGESTSIZE);
 
 
 		ga_filename = apr_psprintf(r->pool,"%s/%s",conf->pwfile,user);
 
-		sharedKey = getSharedKey(r,ga_filename);
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+"GetRealmHash called for sharedkey \"%s\"\n",ga_filename);
+		sharedKey = getSharedKey(r,ga_filename,&static_pw);
 
 		if (!sharedKey)
         return AUTH_USER_NOT_FOUND;
@@ -397,12 +465,20 @@ static authn_status ga_get_realm_hash(request_rec *r, const char *user,
 		uint8_t *secret = get_shared_secret(r,sharedKey,&secretLen);
 
 		unsigned int truncatedHash = computeTimeCode(get_timestamp(),secret,secretLen);
-		char *pwstr = apr_psprintf(r->pool,"%6.6u",truncatedHash);
+		char *pwstr;
+		if (static_pw) {
+			pwstr = apr_psprintf(r->pool,"%s%6.6u",static_pw,truncatedHash);
+		}
+		else 
+		{
+			pwstr = apr_psprintf(r->pool,"%6.6u",truncatedHash);
+		}
 		char *hashstr = apr_psprintf(r->pool,"%s:%s:%s",user,realm,pwstr);
-#ifdef DEBUG
+
+if (conf->debugLevel)
     ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
       "Password \"%s\" at modulus %lu",pwstr,(apr_time_now() / 1000000) % 30);
-#endif
+
 		apr_md5(hash ,hashstr,strlen(hashstr));
 		*rethash = hex_encode(r->pool,hash,APR_MD5_DIGESTSIZE);
 		addCookie(r,secret,secretLen);
@@ -416,24 +492,28 @@ static authn_status ga_get_realm_hash(request_rec *r, const char *user,
 		 ***/
 
 static int do_cookie_auth(request_rec *r) {
-#ifdef DEBUG
-    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
-      "**** COOKIE AUTH at  T=%lu",apr_time_now()/1000000);
-#endif
 
 		char *user;
 
     authn_google_config_rec *conf = ap_get_module_config(r->per_dir_config,
                                                        &authn_google_module);
 
+if (conf->debugLevel)
+    ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+      "**** COOKIE AUTH at  T=%lu",apr_time_now()/1000000);
 		if (conf->cookieLife && find_cookie(r,&user,0L,0L)) {
-#ifdef DEBUG
+
+if (conf->debugLevel)
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
                       "User %s auth granted from cookie",user);
-#endif
+
 			r->user = user;
 			r->ap_auth_type = "Cookie";
 			return OK;
+		} else {
+if (conf->debugLevel)
+        ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r,
+                      "Cookie auth is DECLINED");
 		}
 	return DECLINED;	/* Let someone else deal with it */
 }
@@ -448,6 +528,44 @@ static void ga_child_init (apr_pool_t *p, server_rec *s) {
 		// Each field containing one or more characters which are not cookie-delimiters (comma or semicolon).
 		
 		cookie_regexp= ap_pregcomp(p, "(^|[;,][ \t]*)google_authn[ \t]*=[ \t]*([^;,]+):([^;,]+):([^;,]+)", AP_REG_EXTENDED);
+		passwd_regexp= ap_pregcomp(p, "^\\s*\"\\s*PASSWORD\\s*=\\s*(\\S+)", AP_REG_EXTENDED);
+
+#ifdef TEST_PASS
+		password_test(p," \" PASSWORD=abc ");
+		password_test(p," \" PASSWORD =abc ");
+		password_test(p," \" PASSWORD = abc ");
+		password_test(p," \" PASSWORD= abc ");
+		password_test(p," \"PASSWORD=abc ");
+		password_test(p," \"PASSWORD =abc ");
+		password_test(p," \"PASSWORD = abc ");
+		password_test(p," \"PASSWORD= abc ");
+		password_test(p,"\" PASSWORD=abc ");
+		password_test(p,"\" PASSWORD =abc ");
+		password_test(p,"\" PASSWORD = abc ");
+		password_test(p,"\" PASSWORD= abc ");
+		password_test(p,"\"PASSWORD=abc ");
+		password_test(p,"\"PASSWORD =abc ");
+		password_test(p,"\"PASSWORD = abc ");
+		password_test(p,"\"PASSWORD= abc ");
+		password_test(p," \" PASSWORD=abc");
+		password_test(p," \" PASSWORD =abc");
+		password_test(p," \" PASSWORD = abc");
+		password_test(p," \" PASSWORD= abc");
+		password_test(p," \"PASSWORD=abc");
+		password_test(p," \"PASSWORD =abc");
+		password_test(p," \"PASSWORD = abc");
+		password_test(p," \"PASSWORD= abc");
+		password_test(p,"\" PASSWORD=abc");
+		password_test(p,"\" PASSWORD =abc");
+		password_test(p,"\" PASSWORD = abc");
+		password_test(p,"\" PASSWORD= abc");
+		password_test(p,"\"PASSWORD=abc");
+		password_test(p,"\"PASSWORD =abc");
+		password_test(p,"\"PASSWORD = abc");
+		password_test(p,"\"PASSWORD= abc");
+		cookie_test(p,"google_authn=a:b:c");
+		cookie_test(p,"pma_lang=en; pma_collation_connection=utf8_general_ci; pma_navi_width=200; phpMyAdmin=dsofhasouphf8975q98u; google_authn=lucky:3692581470:9fZZrZ6kMaL2dwXzrfTsqIWdi/c=:; __utma=68896193.834633149.1320516436.1357578935.1357599597.89;__utmz=68896193.1352322698.7.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmc=68896193; member_id=3; pass_hash=09812387643abcdef; ipsconnect_0989875asfdsapofjkjlnsdfau=1");
+#endif
 
 }
 
